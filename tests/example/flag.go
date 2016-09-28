e423efe40af84794db33b2285e434c9df3183395
luckymrwang <luckymrwang@163.com>
2016-09-28 12:06:41 +0800

    添加example

---
+++ tests/example/flag.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+)
+
+func main() {
+
+	//第一个参数，为参数名称，第二个参数为默认值，第三个参数是说明
+
+	wordPtr := flag.String("word", "foo", "a string")
+
+	numbPtr := flag.Int("numb", 42, "an int")
+	boolPtr := flag.Bool("fork", false, "a bool")
+
+	var svar string
+	flag.StringVar(&svar, "svar", "bar", "a string var")
+
+	flag.Parse()
+
+	fmt.Println("word:", *wordPtr)
+	fmt.Println("numb:", *numbPtr)
+	fmt.Println("fork:", *boolPtr)
+	fmt.Println("svar:", svar)
+	fmt.Println("tail:", flag.Args())
+}
--- tests/example/hello.go
+++ tests/example/hello.go
@@ -7,11 +7,11 @@ import (
 func main() {
 	//	fmt.Printf("Hello, world\n")
 	str := "Hello"
+	c := []byte(str)
+	fmt.Println(c)
 	n := len(str)
 	for i := 0; i < n; i++ {
-		//		ch := str[i] // 依据下标取字符串中的字符,类型为byte
-		ch := str[i:i]
-
+		ch := str[i] // 依据下标取字符串中的字符,类型为byte
 		fmt.Println(i, ch)
 
 	}
