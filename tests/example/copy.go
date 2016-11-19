354d53498de427562b1f50e9f13037f22234062d
luckymrwang <luckymrwang@163.com>
2016-11-19 22:14:33 +0800

    更新文档

--- tests/example/copy.go
+++ tests/example/copy.go
@@ -9,6 +9,9 @@ func main() {
 
 	slice1 = make(map[string]string)
 	slice1["hello"] = "boy"
+	slice1["hello"] = "boy"
+	slice1["he"] = "girl"
+	slice1["he"] = "girl"
 	fmt.Println("slice1:", slice1)
 
 	var a = [5]int{1, 2, 3, 4, 5}
