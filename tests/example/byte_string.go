ae1205ce64a5918dc5ec32778f4d70f651d6a36e
luckymrwang <luckymrwang@163.com>
2017-06-01 11:18:55 +0800

    byte&rune

--- tests/example/byte_string.go
+++ tests/example/byte_string.go
@@ -15,4 +15,15 @@ func main() {
 
 	r := []rune(s2)
 	fmt.Println(r) // [20013 25991], 每个字一个数值
+
+	str := "Hello,世界"
+	n := len(str)
+	for i := 0; i < n; i++ {
+		ch := str[i]
+		fmt.Println(i, ch) // ch的类型byte
+	}
+
+	for i, ch := range str {
+		fmt.Println(i, ch) // ch的类型为rune
+	}
 }
