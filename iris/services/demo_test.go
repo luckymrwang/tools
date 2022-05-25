a2d12654ea376f0021c50767c0d128b2e6aef2b7
luckymrwang <luckymrwang@163.com>
2022-05-25 14:09:19 +0800

    add k8s match

--- iris/services/demo_test.go
+++ iris/services/demo_test.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"log"
 	"net"
+	"regexp"
 	"strconv"
 	"testing"
 	"unicode/utf8"
@@ -57,4 +58,20 @@ func TestAS(t *testing.T) {
 
 	f := float64(30.33)
 	fmt.Printf("%.f", f)
+}
+
+func TestMatch(t *testing.T) {
+	const qnameCharFmt string = "[A-Za-z0-9]"
+	const qnameExtCharFmt string = "[-A-Za-z0-9_.]"
+	const qualifiedNameFmt string = "(" + qnameCharFmt + qnameExtCharFmt + "*)?" + qnameCharFmt
+	const qualifiedNameErrMsg string = "must consist of alphanumeric characters, '-', '_' or '.', and must start and end with an alphanumeric character"
+	const qualifiedNameMaxLength int = 63
+
+	var qualifiedNameRegexp = regexp.MustCompile("^" + qualifiedNameFmt + "$")
+	name := `a/b-d`
+	if !qualifiedNameRegexp.MatchString(name) {
+		fmt.Println("not match")
+	} else {
+		fmt.Println("match")
+	}
 }
